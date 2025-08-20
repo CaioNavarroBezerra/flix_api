47f0591815f4fa4a7fd7cda2d41a97284f6afd5b
CaioNavarroBezerra <caionavarro03@gmail.com>
2025-08-20 17:39:58 -0300

    First commit

---
+++ actors/management/commands/import_actors.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from django.core.management.base import BaseCommand
+from actors.models import Actor
+import csv
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'file_name',
+            type=str,
+            help='Nome do arquivo CSV com atores',
+        )
+    
+    def handle(self, *args, **options):
+        file_name = options['file_name']
+
+        with open(file_name, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row['name']
+                birthday = datetime.strftime(row['birthday'], '%Y-%m-%d').date()
+                nationality = row['nationality']
+
+                self.stdout.write(self.style.NOTICE(name))
+
+                Actor.objects.create(
+                    name=name,
+                    birthday=birthday,
+                    nationality=nationality,
+                )
+            
+            self.stdout.write(self.style.SUCCESS('ATORES IMPORTADOS COM SUCESSO'))
